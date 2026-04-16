a43902df255e50d08c04ee43b69b031e56c79012
mnlrioiv <mnl_rioiv@yahoo.com.ar>
2026-04-16 12:31:35 -0300

    Clean deployment: config via .env, no secrets

--- workflows/check_dependency.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-db = sqlite3.connect("/opt/sural/automation/n8n_data/database.sqlite")
-cur = db.cursor()
-cur.execute("SELECT * FROM workflow_dependency")
-for r in cur.fetchall():
-    print(r)
-db.close()
--- workflows/check_user3.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-db = sqlite3.connect("/opt/sural/automation/n8n_data/database.sqlite")
-cur = db.cursor()
-cur.execute("SELECT id, email, roleSlug FROM user")
-for r in cur.fetchall():
-    print(f"user: id={r[0]} email={r[1]} roleSlug={r[2]}")
-db.close()
